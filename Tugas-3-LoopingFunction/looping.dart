7dcd107ffaa598a2fa22f8b62b54ff7b4ea75ee2
fajaragustian <12173512@bsi.ac.id>
2024-03-27 10:10:33 +0700

    Tugas 3 Materi Looping dan Function

---
+++ Tugas-3-LoopingFunction/looping.dart
@@ -0,0 +1,78 @@
+// While
+// while([Kondisi]) { // Kondisi yang menentukan apakah program akan melakukan iterasi. 
+//  Berupa boolean atau true/false.
+//   [Proses] // Merupakan proses yang akan dijalankan dalam satu iterasi
+// }
+
+// Contoh Looping While-loop 1 Looping Angka 1-9 Sederhana
+// void main () {
+//     var flag = 1;
+//     varible flag kurang dari 10
+//     while(flag < 10) {
+//         print ("iterasi ke"+ flag.toString()); mengubah variable flag jadi string
+//         flag++;  Mengubah nilai flag dengan menambahkan 1 //increment
+//     }
+// }
+// Contoh Looping While-loop 2 Looping Mengembalikan Angka Total
+// void main() {
+//   var deret = 5;
+//   var jumlah = 0;
+//   while (deret > 0) {
+//     Loop akan terus berjalan selama nilai deret masih di atas 0
+//     jumlah += deret; // Menambahkan nilai variable jumlah dengan angka deret
+//     deret--; // Mengubah nilai deret dengan mengurangi 1 // decrement
+//     print('Jumlah saat ini: ' + jumlah.toString());
+//   }
+
+//   print(jumlah);
+// }
+
+// For Looop
+// for([Inisialisasi], [Kondisi], [Incremental/Decremental]) {
+//   [Proses] // Merupakan proses yang akan dijalankan dalam satu iterasi
+// } 
+
+// Contoh Looping For-loop 1 Looping Angka 1-9 Sederhana
+// void main(){
+//  for(var angka = 1; angka < 10; angka++) {
+//   print('Iterasi ke-' + angka.toString());
+//  } 
+// }
+
+// Contoh Looping For-loop 2 Looping Mengembalikan Angka Total
+// void main() {
+//   var jumlah = 0;
+//   for (var deret = 5; deret > 0; deret--) {
+//     jumlah += deret;
+//     print('Jumlah saat ini: ' + jumlah.toString());
+//   }
+//   print('Jumlah terakhir: ' + jumlah.toString());
+// }
+
+// Contoh Looping For-loop 3 Looping Dengan Increment dan Decrement Lebih dari 1
+
+// void main() {
+//   variable deret  = 15 kurang dari 10  + dari sama dengan 2  jadinya
+//   for (var deret = 0 ; deret < 10; deret += 2) {
+//     print('Iterasi dengan Increment counter 2: ' + deret.toString());
+//   }
+//   print('-------------------------------------');
+//   variable deret  = 15 lebih dari 0  - dari sama dengan 3 dari angka 15
+//   for (var deret = 15; deret > 0; deret -= 3) {
+//     print('Iterasi dengan Decrement counter : ' + deret.toString());
+
+//   }
+
+// }
+
+// Aborted
+
+// void main() {
+
+//   var flag = 1;
+//   while (flag < 10) {
+//     Loop akan terus berjalan, karena nilai flag tidak pernah berubah
+//     print('Iterasi ke-' + flag.toString());
+//   }
+// }
+
